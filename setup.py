55d03cb582f59efd701f6254ec7b355b64c67639
HackXIt <hackxit@gmail.com>
2023-10-05 19:37:26 +0200

    Adjust setup.py and fix requirements.txt for Kivy[base]

--- setup.py
+++ setup.py
@@ -3,22 +3,23 @@ File: setup.py
 Created on: Thursday, 2023-10-05 @ 19:27:12
 Author: HackXIt (<hackxit@gmail.com>)
 -----
-Last Modified: Thursday, 2023-10-05 @ 19:29:05
+Last Modified: Thursday, 2023-10-05 @ 19:32:01
 Modified By:  HackXIt (<hackxit@gmail.com>) @ HACKXIT
 -----
 '''
 import os
+import platform
 import subprocess
 
-# Name of the virtual environment
-env_name = "env"
+def create_venv():
+    venv_cmd = ['python', '-m', 'venv', 'venv']
+    if platform.system() == 'Windows':
+        python_activation = 'venv\\Scripts\\activate'
+    else:
+        python_activation = 'source venv/bin/activate'
+    subprocess.run(venv_cmd)
+    subprocess.run(python_activation, shell=True)
+    subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
 
-# Create the virtual environment
-subprocess.call(f'python -m venv {env_name}', shell=True)
-
-# Activate the virtual environment
-activate_this = os.path.join(env_name, 'bin', 'activate_this.py')
-exec(open(activate_this).read(), {'__file__': activate_this})
-
-# Install packages from requirements.txt
-subprocess.call(f'{env_name}/bin/pip install -r requirements.txt', shell=True)
+if __name__ == '__main__':
+    create_venv()
