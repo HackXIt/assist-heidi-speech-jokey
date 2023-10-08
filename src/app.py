e5614d3098b6249e01dce4ad4429d15e4d9774fc
HackXIt <hackxit@gmail.com>
2023-10-08 11:41:57 +0200

    I wrote a comment

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ File: app.py
 Created on: Thursday, 2023-10-05 @ 16:22:13
 Author: HackXIt (<hackxit@gmail.com>)
 -----
-Last Modified: Thursday, 2023-10-05 @ 19:24:29
+Last Modified: Sunday, 2023-10-08 @ 11:41:30
 Modified By:  HackXIt (<hackxit@gmail.com>) @ HACKXIT
 -----
 '''
@@ -11,8 +11,10 @@ from kivy.app import App
 from kivy.uix.splitter import Splitter
 from kivy.uix.label import Label
 
+
 class SplitViewApp(App):
     def build(self):
+        # Create a splitter and add two labels
         splitter = Splitter(sizable_from = 'left')
         splitter.add_widget(Label(text = 'Panel 1', size_hint = (0.6, 1)))
 
@@ -22,6 +24,7 @@ class SplitViewApp(App):
         splitter.add_widget(splitter2)
 
         return splitter
+
 
 if __name__ == "__main__":
     SplitViewApp().run()
