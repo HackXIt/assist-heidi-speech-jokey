f315509b109f89cc13dce5c1020bfc0719710332
HackXIt <hackxit@gmail.com>
2023-11-27 21:11:13 +0100

    Yup, still not fixed

--- .github/scripts/windows/build_exe.ps1
+++ .github/scripts/windows/build_exe.ps1
@@ -1,24 +1,30 @@
 # PowerShell script to build Kivy application with Poetry and PyInstaller
 
+Write-Output "listing contents of cwd"
+Get-ChildItem -Force | Out-String
+
+Write-Output 'INFO: Beginning execution'
+
 # Download and install Python
 Write-Output 'INFO: Downloading Python 3.11.6'
 Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.11.6/python-3.11.6-amd64.exe' -OutFile 'python3.11.6.exe'
 Write-Output 'INFO: Installing Python'
-Start-Process -FilePath '.\python3.11.6.exe' -ArgumentList '/quiet InstallAllUsers=1 PrependPath=1 TargetDir=C:\Python311' -Wait -NoNewWindow | Out-String
+Start-Process -FilePath '.\python3.11.6.exe' -ArgumentList '/quiet InstallAllUsers=1 IncludePip=1 PrependPath=1 TargetDir=C:\Python311' -Wait -NoNewWindow | Out-String
 
 # Refresh environment variables to recognize newly installed Python
 $env:Path = [System.Environment]::GetEnvironmentVariable('Path','Machine')
 
 # Install Poetry
 Write-Output 'INFO: Installing Poetry'
-Invoke-Expression (Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content
+pip install poetry
 
 # Set KIVY_GL_BACKEND to angle_sdl2 for compatibility
 $env:KIVY_GL_BACKEND = 'angle_sdl2'
 
 # Install dependencies with Poetry
 Write-Output 'INFO: Installing dependencies with Poetry'
-poetry install
+poetry config virtualenvs.create false
+poetry install --only main --no-root
 
 # Create SpeechJokey.spec file
 $Target = 'SpeechJokey'
